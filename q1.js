d7f5b8292d79ab4cba20c9fc3d1f8f5f31fb8f5f
Teena kanawar <103328068+teena92084@users.noreply.github.com>
2023-06-15 12:37:09 +0530

    Add files via upload

---
+++ q1.js
@@ -0,0 +1,14 @@
+var readline=require('readline-Sync')
+num=readline.question("enter the nuber")
+s=24
+b=8
+
+if (a%2==0 && a%8==0){
+	if (s==a){
+		console.log("Yes")
+		
+}
+	else{
+		print("no")	
+		}
+}
